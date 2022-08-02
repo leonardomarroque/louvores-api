53d3d37dabd08da1d18a158b465c4e2862513d9c
leonardomarroque <leonardo.marroque@gmail.com>
2022-08-02 19:24:56 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ app: "Hello World!" });
+});
+
+app.listen(PORT);
